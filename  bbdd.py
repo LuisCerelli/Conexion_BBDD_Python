00a08b2d3f37f53a0aa6ec1ba30c50f8ec254003
LuisCerelli <cerluisalberto@gmail.com>
2024-10-04 17:29:59 +0200

    subiendo

---
+++  bbdd.py
@@ -0,0 +1,27 @@
+#descargar la bbdd de https://mariadv.org/download/ y las credenciales suelen ser (mysql -u root)
+
+#pip install mysq-conector-python
+
+import mysql.connector
+
+conexion = mysql.connector.connect(
+    host='localhost',
+    user= 'root',
+    password='Andrea90+',
+    datebase= 'bbdd_prueba'
+)
+
+cursor = conexion.cursor()
+
+nuevo_usuario = ('Juan', 30)
+
+consulta = 'INSERT INTO usuarios(nombre, edad) VALUES (%S,%S)'
+
+cursor.execute(consulta, nuevo_usuario)
+
+conexion.commit()
+
+print('Registro guardado')
+
+conexion.close()
+
